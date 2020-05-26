f43b70cfee1327dac348b8a60d403e37af59dab1
Enzo Daneri <enzodaneri@hotmail.com>
2020-05-26 12:45:03 -0300

    got first covid us stats request done

---
+++ src/data/format.js
@@ -0,0 +1,13 @@
+
+
+function number (num) {
+    if(num === null || num === undefined ) {
+        return 'unknown';
+    }
+
+    return num.toLocaleString();
+}
+
+export default {
+    number
+};
